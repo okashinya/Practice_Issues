274227f4a472c43fe4b64f876f3333c06e5f7381
okashinya <33597782+okashinya@users.noreply.github.com>
2024-12-11 11:30:27 +0900

    サイドバーにホームを追加 課題などの場合はコードアイコンを利用するように変更

--- 課題1/test_issues1/src/app/_components/sidebar.tsx
+++ 課題1/test_issues1/src/app/_components/sidebar.tsx
@@ -11,6 +11,8 @@ import {
 } from "@mui/material";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
 import React from "react";
 
 const drawerWidth = 240;
@@ -31,11 +33,11 @@ const SideBar = () => {
       <Toolbar />
       <Box sx={{ overflow: "auto" }}>
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          {["ホーム", "課題1", "Send email", "Drafts"].map((text, index) => (
             <ListItem key={text} disablePadding>
               <ListItemButton>
                 <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  {text === "ホーム" ? <HomeIcon /> : <CodeIcon />}
                 </ListItemIcon>
                 <ListItemText primary={text} />
               </ListItemButton>
