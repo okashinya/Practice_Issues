1499d9832785d3aeb4a7d7da6ae791ea2482c309
okashinya <33597782+okashinya@users.noreply.github.com>
2024-12-12 14:19:39 +0900

    双方向通信の実装。

--- 課題1/test_issues1/src/app/_components/PostalcodeInputForm.tsx
+++ 課題1/test_issues1/src/app/_components/PostalcodeInputForm.tsx
@@ -3,28 +3,31 @@
 import { subAction } from "./ServerActions/PostalCodeProcess";
 import { TextField,Button,Stack} from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
+import { useFormState } from 'react-dom'
 
 
 const PostalForm = () => {
     
     const initialState = { error: "" };
+    const [state, formAction] = useFormState(subAction,{ message: null })
     
     return (
     <>
 
-        <h2>Server Actions</h2>
+        <h2>郵便番号住所検索</h2>
     <Stack
       component="form"
-      action={subAction}
+      action={formAction}
       sx={{ width: '25ch' }}
       spacing={2}
       noValidate
       autoComplete="off"
       method="POST"
     >
-        <TextField id="standard-basic" name="PostalCode" label="郵便番号を入力" variant="standard" />
+        <TextField id="standard-basic" name="PostalCode" label="郵便番号を入力" variant="outlined" />
         <Button type="submit" variant="contained" endIcon={<SendIcon />}>送信する</Button>
         </Stack>
+        <div>{state.message}</div>
     </>
     );
   };
--- 課題1/test_issues1/src/app/_components/ServerActions/PostalCodeProcess.tsx
+++ 課題1/test_issues1/src/app/_components/ServerActions/PostalCodeProcess.tsx
@@ -1,4 +1,11 @@
 "use server";
-export async function subAction(formData: FormData) {
+type State = {
+    message: string | null
+  }
+export async function subAction(previousState: State ,formData: FormData) {
     console.log(formData.get("PostalCode"));
+    if(formData.get("PostalCode")!= ""){
+    return{message:"awsedrftgyhu"}}
+    else{
+    return{message:"郵便番号を入力してください。"}}
   }
