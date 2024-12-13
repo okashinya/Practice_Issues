bb18977e8f6768d714c6327088e52c7c00b0a3e8
okashinya <33597782+okashinya@users.noreply.github.com>
2024-12-13 13:33:50 +0900

    ほぼ完成

--- 課題1/test_issues1/src/app/_components/PostalcodeInputForm.tsx
+++ 課題1/test_issues1/src/app/_components/PostalcodeInputForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { subAction } from "./ServerActions/PostalCodeProcess";
-import { TextField,Button,Stack} from "@mui/material";
+import { TextField,Button,Stack,Box,Typography} from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
 import { useFormState ,Link} from 'react-dom'
 import { usePathname } from "next/navigation";
@@ -10,7 +10,7 @@ import { usePathname } from "next/navigation";
 const PostalForm = () => {
     const pathname = usePathname();
     const initialState = { error: "" };
-    const [state, formAction] = useFormState(subAction,{ message: null })
+    const [state, formAction] = useFormState(subAction,{ message: [] })
     let currentForm = 
     <>
     <h2>郵便番号住所検索</h2>
@@ -26,18 +26,82 @@ const PostalForm = () => {
     <TextField id="standard-basic" name="PostalCode" label="郵便番号を入力" variant="outlined" />
     <Button type="submit" variant="contained" endIcon={<SendIcon />}>送信する</Button>
     </Stack>
-    <div>{state.message}</div>
+
     </>;
 
-    if(state.message=="true"){
+    if(state.message[0]){
+        const tesr = <><br/>12345</>
         currentForm = <>
         <h2>郵便番号住所検索</h2>
-        <Button variant="contained" endIcon={<SendIcon />} component={Link} href={pathname}>送信する</Button>
+        <Box
+        component="div"
+        sx={(theme) => ({
+          whiteSpace: 'normal',
+          my: 2,
+          p: 1,
+          bgcolor: 'grey.100',
+          color: 'grey.800',
+          border: '1px solid',
+          borderColor: 'grey.300',
+          borderRadius: 2,
+          fontSize: '0.875rem',
+          fontWeight: '700',
+          ...theme.applyStyles('dark', {
+            bgcolor: '#101010',
+            color: 'grey.300',
+            borderColor: 'grey.800',
+          }),
+        })}
+      >
+        <div sx={{ whiteSpace: 'pre-line' }}>以下が検索された住所です。<br/>{state.message[2]}</div>
+      </Box>
+      <Button variant="contained" endIcon={<SendIcon />} component={Link} href={pathname}>再度検索する</Button>
         </>;
+    }else if(state.message[0]!=null){
+    currentForm = 
+    <>
+    <h2>郵便番号住所検索</h2>
+    <Stack
+    component="form"
+    action={formAction}
+    sx={{ width: '25ch' }}
+    spacing={2}
+    noValidate
+    autoComplete="off"
+    method="POST"
+    >
+    <TextField id="standard-basic" name="PostalCode" label="郵便番号を入力" variant="outlined" />
+    <Box
+        component="div"
+        sx={(theme) => ({
+          whiteSpace: 'normal',
+          my: 2,
+          p: 1,
+          bgcolor: 'grey.100',
+          color: 'red',
+          border: '1px solid',
+          borderColor: 'grey.300',
+          borderRadius: 2,
+          fontSize: '0.7rem',
+          fontWeight: '700',
+          ...theme.applyStyles('dark', {
+            bgcolor: '#101010',
+            color: 'grey.300',
+            borderColor: 'grey.800',
+          }),
+        })}
+      >
+        正しい郵便番号を入力してください。<br/>
+        {state.message[1]}
+      </Box>
+    <Button type="submit" variant="contained" endIcon={<SendIcon />}>送信する</Button>
+    </Stack>
+
+    </>;
     }
     return (
         currentForm
-    );
+    )
   };
   
   export default PostalForm;
--- 課題1/test_issues1/src/app/_components/ServerActions/PostalCodeProcess.tsx
+++ 課題1/test_issues1/src/app/_components/ServerActions/PostalCodeProcess.tsx
@@ -3,7 +3,37 @@ type State = {
     message: string | null
   }
 export async function subAction(previousState: State ,formData: FormData) {
-    console.log(formData.get("PostalCode"));
+
+    const currentInput = String(formData.get("PostalCode")).replace("-","")
+    const InputState = String(Number(currentInput)) != "NaN" && currentInput.length == 7
+    let ErrorState = ""
+    let resultsAddress = []
+    if(InputState){
+        const res = await fetch("https://zipcloud.ibsnet.co.jp/api/search?zipcode="+currentInput);
+        const getJson = await res.json();
+        console.log(getJson.status)
+        if(getJson.status != 200)
+            ErrorState = "APIエラーです。"
+        for (const value of getJson.results) {
+            resultsAddress.push(<><br/>{value.address1+value.address2+value.address3}</>)
+            
+        }
+        console.log(resultsAddress);
+        //console.log(getJson.results)
+        
+    }
+
+    if(String(Number(currentInput)) != "NaN"){
+        if(currentInput.length < 7 )
+            ErrorState = "桁が足りません。"
+        if(currentInput.length > 7)
+            ErrorState = "桁が多いです。"
+        }else if(String(formData.get("PostalCode")).length > 0){
+            ErrorState = "半角数字を入力してください。"
+        }
+    
+        //console.log(InputState);
+    return {message:[InputState,ErrorState,resultsAddress]}
     if(formData.get("PostalCode")!= ""){
     return{message:"true"}}
     else{
